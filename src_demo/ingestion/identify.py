d93ebd693c04f853db4cd9057743f6fb92ede9f7
Ratul Dash <ratuldash12@gmail.com>
2025-12-17 21:09:07 +0530

    delete quick demo version

--- src_demo/ingestion/identify.py
+++
@@ -1,4 +0,0 @@
-from src_demo.integrations.clay import load_clay_csv
-
-def identify():
-    return load_clay_csv()
--- src_demo/integrations/clay.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_clay_csv(path="src_demo/data/input/clay_leads.csv"):
-    """
-    Clay export should include:
-    name, title, company, linkedin_url, person_location, company_hq, funding_stage
-    """
-    return pd.read_csv(path)
